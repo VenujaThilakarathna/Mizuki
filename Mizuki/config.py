ce4b369860f3baa867fb5f7ab84e3f100849263a
Venuja Thilakarathna <95034872+VenujaThilakarathna@users.noreply.github.com>
2021-11-27 21:43:23 +0530

    Update and rename sample_config.py to config.py

--- Mizuki/config.py
+++ Mizuki/config.py
@@ -14,14 +14,14 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_ID = 7395896  # integer value, dont use ""
+    API_HASH = "cd3998ddf318dad74d7c506731bc0abc"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
+    OWNER_ID = 1826486119  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Venuja_Sadew"
     SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001473463944
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
         -1001190806654
