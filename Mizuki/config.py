6b1cbc5d82bfea7ed6c7b1c486f358189c6b718a
Venuja Thilakarathna <95034872+VenujaThilakarathna@users.noreply.github.com>
2021-11-28 12:33:28 +0530

    Update config.py

--- Mizuki/config.py
+++ Mizuki/config.py
@@ -16,10 +16,10 @@ class Config(object):
 
     API_ID = 7395896  # integer value, dont use ""
     API_HASH = "cd3998ddf318dad74d7c506731bc0abc"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "2109331733:AAG4FwTMj2nzej5hMVFy247IlJT3p-p89u4"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1826486119  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Venuja_Sadew"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "VndBotSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001473463944
     )  # Prints any new group the bot is added to, prints just the name and ID.
@@ -28,24 +28,24 @@ class Config(object):
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgres://frecopip:LlIsZsD-i6OaaWXcW5tihi31H6B725NN@fanny.db.elephantsql.com/frecopip"  # needed for any database modules
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "VB~tIGZv0p8urCZFM4psuaJX03KN9hQFZw7twou7Fd64B~bDzX~VngaxCK_jOxcS"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    DRAGONS = get_user_list("elevated_users.json", "1826486119")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEV_USERS = get_user_list("elevated_users.json", "1826486119")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     DEMONS = get_user_list("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "tigers")
+    TIGERS = get_user_list("elevated_users.json", "1826486119")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
@@ -55,16 +55,16 @@ class Config(object):
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    BAN_STICKER = "CAACAgUAAxkBAAIwfWGg7UllDrKruwqh00JcYyCEKrVBAAKVAwACk9MAAVUCg1HqJDy70CIE"  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "XPGUJ8BSXNBRRQVF"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
     TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = "00850f78b2f05aafbf7f1477058abd2f7ff385d0d8ad73a0b86c3d71010b5f08291d540fdce195d7408121bbea91dd068492e617f5e182c0b20256ef47a2bbda"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
